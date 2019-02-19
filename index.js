fc6cb3b0c81be4ad263667946a30c64e5331b041
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-19 12:51:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+function takeANumber(line, name) {
+ line.push(name);
+return (`Welcome, ${name}. You are number ${line.length} in line.`);
+} 
+function nowServing(katzDeli) {
+var i=0;
+while(i < katzDeli.length) {i++}
+if (katzDeli.length === 0) {
+return 'There is nobody waiting to be served!';
+} else {
+return (`Currently serving ${katzDeli.shift()}.`);
+}
+}
 
+function currentLine(katzDeli) {
+var line = [];
+for (var i=0; line < currentLine.length; i++) 
+if (katzDeli.length === 0) {
+return 'The line is currently empty.';
+} else {
+return ('The line is currently:' + line);
+}
+}
 
